c9fce27f64cbcf44088d9045b039809708f22535
Michal Blaha <michal@michalblaha.cz>
2019-10-14 16:30:23 +0200

    nasi politici prototyp

---
+++ Web.Statu/Controllers/NasiPoliticiController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Devmasters.Core;
+using HlidacStatu.Lib;
+using HlidacStatu.Lib.Data;
+using HlidacStatu.Lib.Data.External.Zabbix;
+using HlidacStatu.Lib.Data.VZ;
+using HlidacStatu.Web.Models;
+using ZabbixApi;
+using static HlidacStatu.Web.Models.ZabbixData;
+
+namespace HlidacStatu.Web.Controllers
+{
+    public class NasiPoliticiController : Controller
+    {
+
+
+        public ActionResult Index()
+        {
+            return RedirectToAction("politik", "NasiPolitici", new { id = "andrej-babis" } );
+        }
+
+        public ActionResult Politik(string id)
+        {
+            HlidacStatu.Lib.Data.Osoba model = HlidacStatu.Lib.Data.Osoby.GetByNameId.Get(id);
+
+            return View(model);
+        }
+
+    }
+}
