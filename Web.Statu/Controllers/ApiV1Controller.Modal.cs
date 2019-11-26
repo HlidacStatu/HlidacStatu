b3cdae2c6e78a5a6e87c15cfc2e42c0f4c671c20
Michal Blaha <michal@michalblaha.cz>
2019-11-26 22:52:15 +0100

    dynamic popup content - beta

---
+++ Web.Statu/Controllers/ApiV1Controller.Modal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Text;
+using System.Web.Mvc;
+
+namespace HlidacStatu.Web.Controllers
+{
+    public partial class ApiV1Controller : Controllers.GenericAuthController
+    {
+        
+
+        public ActionResult ModalClassification(string id, string modalId)
+        {
+            return View(new Tuple<string,string>(id,modalId));
+        }
+
+
+    }
+}
+
+
