9fb5aae51c193a74247d982d50e2fec5d7504887
Michal Blaha <michal@michalblaha.cz>
2020-01-11 13:09:45 +0100

    ignore Highlighting errors

--- Web.Statu/Controllers/DataController.cs
+++ Web.Statu/Controllers/DataController.cs
@@ -272,10 +272,19 @@ namespace HlidacStatu.Web.Controllers
 
                 if (!string.IsNullOrEmpty(this.Request.QueryString["qs"]))
                 {
-                    var findSm = ds.SearchDataRaw($"_id:\"{dataid}\" AND ({this.Request.QueryString["qs"]})", 1, 1,
-                        null, withHighlighting: true);
-                    if (findSm.Total > 0)
-                        ViewBag.Highlighting = findSm.ElasticResultsRaw.Hits.First().Highlight; 
+                    try
+                    {
+                        var findSm = ds.SearchDataRaw($"_id:\"{dataid}\" AND ({this.Request.QueryString["qs"]})", 1, 1,
+                            null, withHighlighting: true);
+                        if (findSm.Total > 0)
+                            ViewBag.Highlighting = findSm.ElasticResultsRaw.Hits.First().Highlight;
+
+                    }
+                    catch (Exception e)
+                    {
+
+                        Util.Consts.Logger.Error("Dataset Detail Highligting query error ", e);
+                    }
 
                 }
 
