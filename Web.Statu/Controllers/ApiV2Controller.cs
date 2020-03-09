d8ec904ca1ef5c4c9a2f70cbcbc1b5b04fdf2f40
suchoss <suchoss@gmail.com>
2020-03-09 17:24:59 +0100

    Adding swashbuckle to project

--- Web.Statu/Controllers/ApiV2Controller.cs
+++ Web.Statu/Controllers/ApiV2Controller.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-
+using System.Web.Mvc;
 
 namespace HlidacStatu.Web.Controllers
 {
     public class ApiV2Controller : GenericAuthController
     {
+        [HttpGet]
+        [Route("/api/v2.0.0/smlouvy/detail/{id}")]
+        [SwaggerOperation("SmlouvyDetailIdGet")]
+        [SwaggerResponse(statusCode: 200, type: typeof(Smlouva), description: "Úspěšně vrácena smlouva")]
+        [SwaggerResponse(statusCode: 400, type: typeof(Chyba), description: "Některé z předaných parametrů byly zadané nesprávně")]
+        [SwaggerResponse(statusCode: 401, type: typeof(Chyba), description: "Nesprávný autorizační token")]
+        [SwaggerResponse(statusCode: 404, type: typeof(Chyba), description: "Požadovaný dokument nebyl nalezen")]
+        [SwaggerResponse(statusCode: 500, type: typeof(Chyba), description: "Došlo k interní chybě na serveru")]
+        public ActionResult SmlouvyDetailIdGet([FromRoute][Required]string id)
+        {
+        }
+
     }
 }
