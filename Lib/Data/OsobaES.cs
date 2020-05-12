c141c8e1f804808988b1ea97142c3f3c8879544b
suchoss <suchoss@gmail.com>
2020-05-12 10:53:32 +0200

    New project to transfer people to ES for better search

---
+++ Lib/Data/OsobaES.cs
@@ -0,0 +1,21 @@
+﻿using Nest;
+
+namespace HlidacStatu.Lib.Data
+{
+    [ElasticsearchType(IdProperty = nameof(OsobaId))]
+    class OsobaES
+    {
+        [Nest.Keyword]
+        public string OsobaId { get; set; }
+        [Nest.Text]
+        public string FullName { get; set; }
+        [Nest.Number]
+        public int BirthYear { get; set; }
+        [Nest.Text]
+        public string[] PoliticalFunctions { get; set; }
+        [Nest.Text]
+        public string PoliticalParty { get; set; }
+        [Nest.Text]
+        public string Status { get; set; }
+    }
+}
