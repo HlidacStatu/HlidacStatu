8501a755eb5c475b12c8237ea2e79ec196266f59
suchoss <suchoss@gmail.com>
2020-05-21 08:55:06 +0200

    update

---
+++ HlidacStatu.Q.ClassificationRepair/ProcessMessage.cs
@@ -0,0 +1,16 @@
+﻿using HlidacStatu.Q.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HlidacStatu.Q.ClassificationRepair
+{
+    public class ProcessMessage : IMessageHandler<ClassificationFeedback>
+    {
+        public void Handle(ClassificationFeedback message)
+        {
+            Console.WriteLine($"Message with id: {message.IdSmlouvy} is being proceesed. Prop cat = {message.ProposedCategories}; sender = {message.FeedbackEmail}");
+        }
+    }
+}
