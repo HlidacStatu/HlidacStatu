2a410c3bb3cbea846a5e8c9447c0990d05e941b2
Michal <michal@michalblaha.cz>
2020-06-03 13:09:45 +0200

    insolvence fix

--- Web.Statu/Framework/InsolvenceLimitedView.cs
+++ Web.Statu/Framework/InsolvenceLimitedView.cs
@@ -10,9 +10,10 @@ namespace HlidacStatu.Web.Framework
     public static class InsolvenceLimitedView
     {
         static string[] defaultRolesWithoutLimitation = new string[] { "Admin", "novinar" };
+
         public static bool IsLimited(Controllers.GenericAuthController c, string[] validRoles = null)
         {
-            validRoles = validRoles ?? new string[] { "Admin", "novinar" };
+            validRoles = validRoles ?? defaultRolesWithoutLimitation;
             var usrmgr = HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
 
             if (c.User?.Identity?.IsAuthenticated == true)
@@ -23,6 +24,25 @@ namespace HlidacStatu.Web.Framework
                 foreach (var role in validRoles)
                 {
                     if (c.User.IsInRole(role.Trim()))
+                        return false;
+                }
+                return true;
+            }
+            return true;
+
+        }
+
+        public static bool IsLimited(System.Security.Principal.IPrincipal user, string[] validRoles = null)
+        {
+            validRoles = validRoles ?? defaultRolesWithoutLimitation;
+            if (user?.Identity?.IsAuthenticated == true)
+            {
+                if (validRoles.Count() == 0)
+                    return false;
+
+                foreach (var role in validRoles)
+                {
+                    if (user.IsInRole(role.Trim()))
                         return false;
                 }
                 return true;
