e1400ee68f7a1bd6df7d8a0a7a442dcefec220ee
Michal <michal@michalblaha.cz>
2020-07-27 22:26:41 +0200

    Hartenberg vazby

---
+++ DBUpgrader/V1/UpgradeDB.1.0.0.073.cs
@@ -0,0 +1,32 @@
+﻿using DatabaseUpgrader;
+
+
+namespace HlidacStatu.DBUpgrades
+{
+    public static partial class DBUpgrader
+	{
+
+		private partial class UpgradeDB
+		{
+
+			[DatabaseUpgradeMethod("1.0.0.73")]
+			public static void Init_1_0_0_73(IDatabaseUpgrader du)
+			{
+
+				string sql = @"
+
+--investice hartenberg holding
+insert into FirmaVazby(ico, VazbakICO, datumOd, TypVazby, PojmenovaniVazby, zdroj, RucniZapis)
+values 
+('01801261','02625067','2019-06-19',19,'akcionář','výroční zpráva Hartenberg Holding',1),
+('01801261','04226917','2015-08-10',19,'akcionář','výroční zpráva Hartenberg Holding',1),
+('01801261','26951070','2019-06-19',19,'akcionář','výroční zpráva Hartenberg Holding',1),
+('01801261','25944355','2018-05-21',19,'akcionář','výroční zpráva Hartenberg Holding',1),
+('01801261','27528791','2018-05-21',19,'akcionář','výroční zpráva Hartenberg Holding',1)
+";
+				du.RunDDLCommands(sql);
+
+			}
+		}
+	}
+}
