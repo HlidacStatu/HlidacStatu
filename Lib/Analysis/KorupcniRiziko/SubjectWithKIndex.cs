ede9742182283eda2a6fd757f4cca9c7b7ec0962
suchoss <suchoss@gmail.com>
2020-08-28 12:43:00 +0200

    Porovnani BE part

--- Lib/Analysis/KorupcniRiziko/SubjectWithKIndex.cs
+++ Lib/Analysis/KorupcniRiziko/SubjectWithKIndex.cs
@@ -21,4 +21,37 @@ namespace HlidacStatu.Lib.Analysis.KorupcniRiziko
         public Dictionary<int, decimal> Roky { get; set; }
     }
 
+    public class SubjectWithKIndexAnnualData : Lib.Data.Firma.Zatrideni.Item
+    {
+        public SubjectWithKIndexAnnualData() { }
+        
+        public SubjectWithKIndexAnnualData(Data.Firma.Zatrideni.Item item)
+        {
+            Group = item.Group;
+            Ico = item.Ico;
+            Jmeno = item.Jmeno;
+            Kraj = item.Kraj;
+            KrajId = item.KrajId;
+        }
+
+        public void PopulateWithAnnualData(int year)
+        {
+            if (string.IsNullOrWhiteSpace(Ico))
+                throw new Exception("Ico is missing");
+
+            year = Consts.FixKindexYear(year);
+            var kindex = KIndex.Get(Ico);
+
+            if (kindex != null)
+            {
+                AnnualData = kindex.ForYear(year);
+            }
+            else
+                AnnualData = KIndexData.Annual.Empty(year);
+        }
+            
+        public KIndexData.Annual AnnualData { get; set; }
+
+    }
+
 }
