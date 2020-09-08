8708fc48d09bacf86a38092ec609b18069c793c4
suchoss <suchoss@gmail.com>
2020-09-08 11:21:16 +0200

    Adding Kindex icon to HS

--- Lib/Analysis/KorupcniRiziko/SubjectNameCache.cs
+++ Lib/Analysis/KorupcniRiziko/SubjectNameCache.cs
@@ -84,6 +84,19 @@ namespace HlidacStatu.Lib.Analysis.KorupcniRiziko
                 )
                 .Take(take);
             totalResult = totalResult.Union(tokenSearchAny).Take(take);
+
+            // přesnější, pomalejší než tokenSearchAny, ale zase by to možná mohlo zastoupit token search all
+            //var tokenSearchCount = GetCompanies().Values
+            //    .Select(c => new
+            //    {
+            //        hits = tokenizedSearchInput.Count(i => c.Tokens.Any(tkn => tkn.StartsWith(i))),
+            //        subject = c
+            //    })
+            //    .Where(x => x.hits > 0)
+            //    .OrderByDescending(x => x.hits)
+            //    .Select(x => x.subject)
+            //    .Take(take);
+
             return totalResult;
         }
 
