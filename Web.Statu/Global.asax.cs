b883bdc3bdeae21ca6adc1c0361d4c2cd7139bd8
Michal <michal@michalblaha.cz>
2020-10-11 21:20:22 +0200

    auto block

--- Web.Statu/Global.asax.cs
+++ Web.Statu/Global.asax.cs
@@ -3,6 +3,7 @@
 using Newtonsoft.Json;
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -19,7 +20,11 @@ namespace HlidacStatu.Web
 
         public static volatile MemoryCacheManager<string, string> CachedDatasets = null;
 
+
         public static Devmasters.Cache.LocalMemory.AutoUpdatedLocalMemoryCache<string[]> BannedIPs = null;
+
+        class attack { public int num; public DateTime last; public string ip; }
+        static Dictionary<string, attack> attackers = new Dictionary<string, attack>();
 
         protected void Application_BeginRequest(Object sender, EventArgs e)
         {
@@ -51,6 +56,9 @@ namespace HlidacStatu.Web
                     {
                         using (Devmasters.PersistLib p = new Devmasters.PersistLib())
                         {
+                            p.ExecuteNonQuery(Devmasters.Config.GetWebConfigValue("CnnString"),
+                                 System.Data.CommandType.Text, "delete from BannedIPs where expiration < GetDate()", null);
+
                             var ds = p.ExecuteDataset(Devmasters.Config.GetWebConfigValue("CnnString"),
                                  System.Data.CommandType.Text, "select IP from BannedIPs where Expiration is null or expiration > GetDate()",
                                  null);
@@ -152,7 +160,7 @@ namespace HlidacStatu.Web
                     .SetCustomKeyValue("referrer", this.Request.UrlReferrer != null ? this.Request.UrlReferrer.AbsoluteUri : "")
                     );
 
-                InvokeErrorAction( Controllers.HomeController.ErrorPages.ErrorHack , this.Context, httpException);
+                InvokeErrorAction(Controllers.HomeController.ErrorPages.ErrorHack, this.Context, httpException);
                 goto finishResponse;
             }
             if (exception.GetType() == typeof(System.Web.HttpRequestValidationException)
@@ -171,7 +179,7 @@ namespace HlidacStatu.Web
                     .SetCustomKeyValue("referrer", this.Request.UrlReferrer != null ? this.Request.UrlReferrer.AbsoluteUri : "")
                     );
 
-                    InvokeErrorAction(Controllers.HomeController.ErrorPages.ErrorHack, this.Context, httpException);
+                InvokeErrorAction(Controllers.HomeController.ErrorPages.ErrorHack, this.Context, httpException);
                 goto finishResponse;
             }
             try
@@ -185,7 +193,7 @@ namespace HlidacStatu.Web
                     .SetCustomKeyValue("Url", this.Request?.Url?.AbsoluteUri)
                     );
 
-                    InvokeErrorAction(Controllers.HomeController.ErrorPages.ErrorHack, this.Context, httpException);
+                InvokeErrorAction(Controllers.HomeController.ErrorPages.ErrorHack, this.Context, httpException);
 
             }
             catch (Exception)
@@ -199,8 +207,84 @@ namespace HlidacStatu.Web
             return;
         }
 
+        object _autoAddBlackList = new object();
+        void AutoAddBlackList(Controllers.HomeController.ErrorPages errPage, HttpRequest req)
+        {
+            bool add = false;
+            var dt = DateTime.Now;
+            string ip = req.UserHostAddress;
+            lock (_autoAddBlackList)
+            {
+                if (attackers.ContainsKey(ip))
+                {
+                    attackers[ip].num++;
+                }
+                else
+                    attackers.Add(ip, new attack() { ip = ip, last = DateTime.Now, num = 1 });
+            }
+
+            var att = attackers[ip];
+            var diff = (dt - att.last).TotalSeconds;
+
+            if (diff >= 600)
+                att.num = 1;
+            else
+                att.num = att.num + 1;
+
+            switch (errPage)
+            {
+                case Controllers.HomeController.ErrorPages.Ok:
+                    break;
+                case Controllers.HomeController.ErrorPages.NotFound:
+                    add = att.num > 20;
+                    break;
+                case Controllers.HomeController.ErrorPages.Error:
+                    add = att.num > 15;
+                    break;
+                case Controllers.HomeController.ErrorPages.ErrorHack:
+                    add = att.num > 10;
+                    break;
+                default:
+                    break;
+            }
+
+            if (add)
+            {
+                HlidacStatu.Util.Consts.Logger.Warning($"added bannedIP {ip}");
+                try
+                {
+                    using (Devmasters.PersistLib p = new Devmasters.PersistLib())
+                    {
+                        p.ExecuteNonQuery(Devmasters.Config.GetWebConfigValue("CnnString"),
+                             System.Data.CommandType.Text, "INSERT INTO [dbo].[BannedIPs] ([IP],[Expiration],[Created]) "
+                             + " VALUES(@ip,@dat,GetDate())", new System.Data.IDataParameter[] {
+                            new System.Data.SqlClient.SqlParameter("ip", ip),
+                            new System.Data.SqlClient.SqlParameter("dat", dt.AddHours(6))
+                            });
+                    }
+                    BannedIPs.ForceRefreshCache();
+
+                }
+                catch (Exception e)
+                {
+                    HlidacStatu.Util.Consts.Logger.Warning($"added bannedIP {ip}", e);
+
+                }
+            }
+        }
+
         void InvokeErrorAction(Controllers.HomeController.ErrorPages errPage, HttpContext httpContext, Exception exception)
         {
+            AutoAddBlackList(errPage, httpContext.Request);
+
+            System.Configuration.Configuration configuration = System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration("/");
+            System.Web.Configuration.CustomErrorsSection section = (System.Web.Configuration.CustomErrorsSection)configuration.GetSection("system.web/customErrors");
+
+#if DEBUG
+            return;
+#endif
+
+
             var routeData = new RouteData();
             routeData.Values["controller"] = "Home";
             routeData.Values["action"] = "Error";
@@ -211,7 +295,7 @@ namespace HlidacStatu.Web
             //var httpcx = new HttpContext(new HttpRequest("error", "https://www.hlidacstatu.cz/error",""), httpContext.Response);
 
 
-            Response.Redirect("/Error/"+errPage);
+            Response.Redirect("/Error/" + errPage);
 
             if (false)
             {
