c30f83e97f578e1dcbaa6b19560d648189e97219
Michal <michal@michalblaha.cz>
2020-11-19 12:00:28 +0100

    Update Firma.Statistics.RegistrSmluv.cs

--- Lib/Data/Firma.Statistics.RegistrSmluv.cs
+++ Lib/Data/Firma.Statistics.RegistrSmluv.cs
@@ -25,7 +25,7 @@ namespace HlidacStatu.Lib.Data
                         {
                             registrSmluvCaches.Add(obor.Value,
                                 Util.Cache.CouchbaseCacheManager<Analytics.StatisticsSubjectPerYear<Statistics.RegistrSmluv>, Firma>
-                                .GetSafeInstance("Firma_SmlouvyStatistics_"+obor.Value.ToString(),
+                                .GetSafeInstance("Firma_SmlouvyStatistics_" + obor.Value.ToString(),
                                     (firma) => RegistrSmluv.Create(firma, null),
                                     TimeSpan.FromHours(12), f => f.ICO)
                                 );
@@ -35,6 +35,15 @@ namespace HlidacStatu.Lib.Data
                 return registrSmluvCaches[obor.Value];
             }
 
+            public static decimal PercentOf(long number, long total) => PercentOf((decimal)number, (decimal)total);
+            public static decimal PercentOf(double number, double total) => PercentOf((decimal)number, (decimal)total);
+
+            public static decimal PercentOf(decimal number, decimal total)
+            {
+                if (total == 0)
+                    return 0;
+                return number / total;
+            }
 
             public partial class RegistrSmluv
             {
@@ -68,14 +77,29 @@ namespace HlidacStatu.Lib.Data
                 public static Analytics.StatisticsSubjectPerYear<RegistrSmluv> Create(Firma f, int? obor)
                 {
 
-                    Dictionary<int, Lib.Analysis.BasicData> _calc_SeZasadnimNedostatkem = Lib.ES.QueryGrouped.SmlouvyPerYear($"ico:{f.ICO} and chyby:zasadni", Lib.Analytics.Consts.RegistrSmluvYearsList);
+                    Dictionary<int, Lib.Analysis.BasicData> _calc_SeZasadnimNedostatkem = 
+                        Lib.ES.QueryGrouped.SmlouvyPerYear($"ico:{f.ICO} and chyby:zasadni", Lib.Analytics.Consts.RegistrSmluvYearsList);
 
-                    Dictionary<int, Lib.Analysis.BasicData> _calc_UzavrenoOVikendu = Lib.ES.QueryGrouped.SmlouvyPerYear($"ico:{f.ICO} AND (hint.denUzavreni:>0)", Lib.Analytics.Consts.RegistrSmluvYearsList);
+                    Dictionary<int, Lib.Analysis.BasicData> _calc_UzavrenoOVikendu = 
+                        Lib.ES.QueryGrouped.SmlouvyPerYear($"ico:{f.ICO} AND (hint.denUzavreni:>0)", Lib.Analytics.Consts.RegistrSmluvYearsList);
 
-                    Dictionary<int, Lib.Analysis.BasicData> _calc_ULimitu = Lib.ES.QueryGrouped.SmlouvyPerYear($"ico:{f.ICO} AND ( hint.smlouvaULimitu:>0 )", Lib.Analytics.Consts.RegistrSmluvYearsList);
+                    Dictionary<int, Lib.Analysis.BasicData> _calc_ULimitu = 
+                        Lib.ES.QueryGrouped.SmlouvyPerYear($"ico:{f.ICO} AND ( hint.smlouvaULimitu:>0 )", Lib.Analytics.Consts.RegistrSmluvYearsList);
 
-                    Dictionary<int, Lib.Analysis.BasicData> _calc_NovaFirmaDodavatel = Lib.ES.QueryGrouped.SmlouvyPerYear($"ico:{f.ICO} AND ( hint.pocetDniOdZalozeniFirmy:>-50 AND hint.pocetDniOdZalozeniFirmy:<30 )", Lib.Analytics.Consts.RegistrSmluvYearsList);
+                    Dictionary<int, Lib.Analysis.BasicData> _calc_NovaFirmaDodavatel = 
+                        Lib.ES.QueryGrouped.SmlouvyPerYear($"ico:{f.ICO} AND ( hint.pocetDniOdZalozeniFirmy:>-50 AND hint.pocetDniOdZalozeniFirmy:<30 )", Lib.Analytics.Consts.RegistrSmluvYearsList);
 
+                    Dictionary<int, Lib.Analysis.BasicData> _calc_smlouvy = 
+                        Lib.ES.QueryGrouped.SmlouvyPerYear($"ico:{f.ICO} ", Lib.Analytics.Consts.RegistrSmluvYearsList);
+
+                    Dictionary<int, Lib.Analysis.BasicData> _calc_bezCeny = 
+                        Lib.ES.QueryGrouped.SmlouvyPerYear($"ico:{f.ICO} AND ( issues.issueTypeId:100 ) ", Lib.Analytics.Consts.RegistrSmluvYearsList);
+
+                    Dictionary<int, Lib.Analysis.BasicData> _calc_bezSmlStran =
+                        Lib.ES.QueryGrouped.SmlouvyPerYear($"ico:{f.ICO} AND ( issues.issueTypeId:18 OR issues.issueTypeId:12 ) ", Lib.Analytics.Consts.RegistrSmluvYearsList);
+
+                    Dictionary<int, Lib.Analysis.BasicData> _calc_sVazbouNaPolitikyNedavne =
+                        Lib.ES.QueryGrouped.SmlouvyPerYear($"ico:{f.ICO} AND ( sVazbouNaPolitikyNedavne:true ) ", Lib.Analytics.Consts.RegistrSmluvYearsList);
 
                     Dictionary<int, RegistrSmluv> data = new Dictionary<int, RegistrSmluv>();
                     foreach (var year in Lib.Analytics.Consts.RegistrSmluvYearsList)
@@ -83,22 +107,24 @@ namespace HlidacStatu.Lib.Data
                         var stat = f.Statistic().RatingPerYear[year];
                         data.Add(year, new RegistrSmluv()
                         {
-                            PocetSmluv = f.Statistic().BasicStatPerYear[year].Pocet,
-                            CelkovaHodnotaSmluv = f.Statistic().BasicStatPerYear[year].CelkemCena,
-                            PocetSmluvBezCeny = stat.NumBezCeny,
-                            PocetSmluvBezSmluvniStrany = stat.NumBezSmluvniStrany,
-                            PocetSmluvPolitiky = stat.NumSPolitiky,
-                            PercentSmluvBezCeny = stat.PercentBezCeny,
-                            PercentSmluvBezSmluvniStrany = stat.PercentBezSmluvniStrany,
-                            PercentKcBezSmluvniStrany = stat.PercentKcBezSmluvniStrany,
-                            PercentKcSmluvPolitiky = stat.PercentKcSPolitiky,
-                            PercentSmluvPolitiky = stat.PercentSPolitiky,
-                            SumKcSmluvBezSmluvniStrany = stat.SumKcBezSmluvniStrany,
-                            SumKcSmluvPolitiky = stat.SumKcSPolitiky,
+                            PocetSmluv = _calc_smlouvy[year].Pocet,
+                            CelkovaHodnotaSmluv = _calc_smlouvy[year].CelkemCena,
+                            PocetSmluvBezCeny = _calc_bezCeny[year].Pocet,
+                            PocetSmluvBezSmluvniStrany = _calc_bezSmlStran[year].Pocet,
+                            PocetSmluvPolitiky = _calc_sVazbouNaPolitikyNedavne[year].Pocet,
+                            SumKcSmluvBezSmluvniStrany = _calc_bezSmlStran[year].Pocet,
+                            SumKcSmluvPolitiky = _calc_sVazbouNaPolitikyNedavne[year].CelkemCena,
                             PocetSmluvULimitu = _calc_ULimitu[year].Pocet,
                             PocetSmluvOVikendu = _calc_UzavrenoOVikendu[year].Pocet,
                             PocetSmluvSeZasadnimNedostatkem = _calc_SeZasadnimNedostatkem[year].Pocet,
                             PocetSmluvNovaFirma = _calc_NovaFirmaDodavatel[year].Pocet,
+
+                            PercentSmluvBezCeny = PercentOf(_calc_bezCeny[year].Pocet, _calc_smlouvy[year].Pocet),
+                            PercentSmluvBezSmluvniStrany = PercentOf(_calc_bezSmlStran[year].Pocet, _calc_smlouvy[year].Pocet),
+                            PercentKcBezSmluvniStrany = PercentOf(_calc_bezCeny[year].CelkemCena, _calc_smlouvy[year].CelkemCena),
+                            PercentKcSmluvPolitiky = PercentOf(_calc_sVazbouNaPolitikyNedavne[year].CelkemCena, _calc_smlouvy[year].CelkemCena),
+                            PercentSmluvPolitiky = PercentOf(_calc_sVazbouNaPolitikyNedavne[year].Pocet, _calc_smlouvy[year].Pocet),
+
                         }
                         );
                     }
